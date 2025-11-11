d7d4d7b3f0cf476d2c7e047595e9ac1ec9139271
Matthias Dumke <matthias.dumke@gmx.net>
2025-11-11 15:46:27 +0100

    feat: implement single door to open

--- src/main.js
+++ src/main.js
@@ -1,5 +1,10 @@
 import './components/pan-container/pan-container.js'
 
-document.getElementById('door-04').addEventListener('click', () => {
-  console.log('Door 04 clicked')
+document.getElementById('door-04').addEventListener('click', e => {
+  console.log('door 04 clicked')
+  e.currentTarget.classList.add('open')
+})
+
+document.getElementById('door-04-content').addEventListener('click', () => {
+  console.log('play door 04 content')
 })
